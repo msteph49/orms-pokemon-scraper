b7fbcf80e086f0dbf43147b9457a6b4f8c19adff
Michael Stephens <mdstephens26@yahoo.com>
2021-02-06 23:26:07 -0500

    commit tree fcdea7a1c1060ca88fed0f12e36753ad9d525daa

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,16 @@
 class Pokemon
+    attr_accessor :id, :name, :type, :db
+    def initialize(id:, name:, type:, db:)
+        @id = id
+        @name = name
+        @type = type
+        @db = db
+    end
+    def self.save(name, type, db)
+        db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)", name, type)
+    end
+    def self.find(id_number, db)
+        pokemon = db.execute("SELECT * FROM pokemon WHERE id = ?", id_number).flatten
+        Pokemon.new(id: pokemon[0], name: pokemon[1], type: pokemon[2], db: db)
+    end
 end
